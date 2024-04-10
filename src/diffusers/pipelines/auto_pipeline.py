9a0575c7c067f21b66ceaaad2830ea7ccb8c0eab
Saravana Rathinam <sr@elbo.ai>
2024-04-10 12:11:59 -0700

    Import fixes

--- src/diffusers/pipelines/auto_pipeline.py
+++ src/diffusers/pipelines/auto_pipeline.py
@@ -160,7 +160,7 @@ def _get_task_class(mapping, pipeline_class_name, throw_error_if_not_exist: bool
 
     model_name = get_model(pipeline_class_name)
     if model_name is None and pipeline_class_name == "StableDiffusionHighResFixPipeline":
-        return StableDiffusionHighResPipeline
+        return StableDiffusionHighResFixPipeline
 
     if model_name is not None:
         task_class = mapping.get(model_name, None)
